d71b1088c03dcac3f60554c9c9d6692696b9bee1
Andrew Catton <andrew@catton.ca>
2017-05-30 11:21:55 -0400

    Don't have separate ActionsApi - instead, just allow `new EllipsisApi(ellipsis)`

---
+++ error-messages.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ELLIPSIS_OBJECT_MISSING: "You need to pass an `ellipsis` object through from an Ellipsis action",
+  MESSAGE_MISSING: "You need to pass a `message` argument",
+  ACTION_NAME_MISSING: "You need to pass an `actionName` argument",
+  TRIGGER_AND_ACTION_NAME_MISSING: "You need to pass either an `actionName` or a `trigger` argument",
+  BOTH_TRIGGER_AND_ACTION_NAME: "You can't pass both an `actionName` and a `trigger` argument. Just pass one",
+  SCHEDULE_ACTION_MISSING: "You need to pass an `action` argument for the thing you want to schedule",
+  UNSCHEDULE_ACTION_MISSING: "You need to pass an `action` argument for the thing you want to unschedule",
+  RECURRENCE_MISSING: "You need to pass a `recurrence` argument to specify when you want to schedule the action to recur, e.g. \"every weekday at 9am\""
+};
